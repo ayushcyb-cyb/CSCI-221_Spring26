aba33b2d8b077569d9ba894a348999544d0d76b4
ayushcyb-cyb <aayush.yadav@go.stcloudstate.edu>
2026-01-24 06:08:42 +0000

    Module_2 assignnment

---
+++ 2_module2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 20;
+    float b = 5.8f;
+    char c = 'A';
+
+    printf("A: %d\n", a);
+    printf("B: %.1f\n", b);
+    printf("C: %c\n", c);
+
+    return 0;
+}
---
+++ 4_module2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    
+    int number;
+    int i;
+    
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    
+  
+    if (number % 2 == 0) {
+
+        printf("\nThe number is Even\n");
+        printf("Printing 5 times:\n");
+
+        for (i = 1; i <= 5; i++) {
+            printf("%d. Even\n", i);
+        }
+    } else {
+
+        printf("\nThe number is Odd\n");
+        printf("Printing 5 times:\n");
+
+        for (i = 1; i <= 5; i++) {
+            printf("%d. Odd\n", i);
+        }
+    }
+    
+    return 0;
+}
