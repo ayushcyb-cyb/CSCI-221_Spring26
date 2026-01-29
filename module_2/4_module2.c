cb8553386d727e68d8a3ef32dd25f533131b6dcb
ayushcyb-cyb <aayush.yadav@go.stcloudstate.edu>
2026-01-29 01:34:54 +0000

    organizing

---
+++ module_2/4_module2.c
@@ -0,0 +1,40 @@
+// #include <stdio.h>
+
+// int main() {
+    
+//     int number;
+//     int i;
+    
+//     printf("Enter an integer: ");
+//     scanf("%d", &number);
+    
+  
+//     if (number % 2 == 0) {
+
+//         printf("\nThe number is Even\n");
+//         printf("Printing 5 times:\n");
+
+//         for (i = 1; i <= 5; i++) {
+//             printf("%d. Even\n", i);
+//         }
+//     } else {
+
+//         printf("\nThe number is Odd\n");
+//         printf("Printing 5 times:\n");
+
+//         for (i = 1; i <= 5; i++) {
+//             printf("%d. Odd\n", i);
+//         }
+//     }
+    
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Program is running...\n");
+
+    // pretend something went wrong
+    return 89;   // returning 1 means: program ended with an error
+}
