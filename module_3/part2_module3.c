6ec530cf30353b353dd2eabb30d5986062cf4a47
ayushcyb-cyb <aayush.yadav@go.stcloudstate.edu>
2026-01-31 01:33:18 +0000

    part2 ans

--- module_3/part2_module3.c
+++ module_3/part2_module3.c
@@ -1,1 +1,30 @@
-//code
+// Implement a function sum_ints(const int *a, size_t n) that returns the sum using pointer iteration (no indexing).
+
+#include<stdio.h>
+
+int sum_ints(const int *a, size_t n); //prototyping function
+
+int sum_ints(const int *a, size_t n){
+
+    int sum = 0;
+    const int *end = a + n;
+
+    while (a < end){ //iteration using while loop
+        sum += *a;
+        a++;
+    }
+    return sum;
+
+    
+}
+
+int main(){
+
+    int a[10]={1,2,3,4,5,6,7,8,9,10};
+    size_t n=10;
+
+    int sum = sum_ints(a, n); //calling func.
+    printf("Sum of the ints in array: %d\n", sum);
+
+    return 0;
+}
